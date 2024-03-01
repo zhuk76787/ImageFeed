29a16cac6d63f2e64d7e31c75a3f158347950cce
zhuk76787 <zhuk76787@gmail.com>
2024-03-01 23:58:48 +0400

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Жуков on 3/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
