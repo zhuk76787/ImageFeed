e6b38ba22942397152211fff4f7e23835bf2186e
zhuk76787 <zhuk76787@gmail.com>
2024-07-23 00:29:46 +0400

    рефакторинг ProfileView

--- ImageFeed/Profile/ProfilePresenter.swift
+++ ImageFeed/Profile/ProfilePresenter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+public protocol ProfilePresenterProtocol {
+    var view: ProfileViewControllerProtocol? { get set }
+    func updateProfileDetails()
+    func updateAvatar()
+    func logout()
+}
+
+final class ProfilePresenter: ProfilePresenterProtocol {
+    weak var view: ProfileViewControllerProtocol?
+    private let profileService = ProfileService.shared
+    private let profileImageService = ProfileImageService.shared
+    private let profileLogoutService = ProfileLogoutService.shared
+    
+    init(view: ProfileViewControllerProtocol? = nil) {
+        self.view = view
+    }
+    
+    func updateAvatar() {
+        guard let profileImageURL = ProfileImageService.shared.profileImageURL,
+              let url = URL(string: profileImageURL)
+        else { return }
+        view?.setAvatar(url: url)
+    }
+    
+    func updateProfileDetails() {
+        guard let profile = profileService.profileModel else {
+            print("Try to read: profileService.profileModel")
+            return
+        }
+        view?.updateView(data: profile)
+    }
+    
+    func logout() {
+        profileLogoutService.logout()
+    }
+}
